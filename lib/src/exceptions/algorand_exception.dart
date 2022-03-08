69aceeaf88acc201667067bc889ed577193690b2
Tomas Verhelst <tomasverhelst@gmail.com>
2022-03-08 12:58:19 +0100

    feat: better algorand exception messages

--- lib/src/exceptions/algorand_exception.dart
+++ lib/src/exceptions/algorand_exception.dart
@@ -1,6 +1,23 @@
+import 'package:dio/dio.dart';
+
 class AlgorandException implements Exception {
-  final String message;
+  final String _message;
   final Object? cause;
 
-  AlgorandException({this.message = '', this.cause});
+  AlgorandException({String message = '', this.cause}) : _message = message;
+
+  String get message {
+    final cause = this.cause;
+    if (cause is! DioError) {
+      return _message;
+    }
+
+    final message = cause.response?.data['message'];
+
+    if (message is! String) {
+      return _message;
+    }
+
+    return message;
+  }
 }
