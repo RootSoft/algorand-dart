5321e1c952b1b4bc18e2be82c6865c78c12881a5
ArunBabu98 <arun.b@changeangel.io>
2024-02-04 13:16:12 +0530

    algorand nownodes change -2

--- lib/src/api/responses/accounts/account_lookup_response.g.dart
+++ lib/src/api/responses/accounts/account_lookup_response.g.dart
@@ -9,8 +9,9 @@ part of 'account_lookup_response.dart';
 AccountResponse _$AccountResponseFromJson(Map<String, dynamic> json) =>
     AccountResponse(
       currentRound: json['current-round'] as int?,
-      account:
-          AccountInformation.fromJson(json['account'] as Map<String, dynamic>),
+      account: json['account'] != null
+          ? AccountInformation.fromJson(json['account'] as Map<String, dynamic>)
+          : null,
     );
 
 Map<String, dynamic> _$AccountResponseToJson(AccountResponse instance) =>
